dd24f2ff435d09dd4fcbdf47ac6f4bd336b61f40
Đinh Bình Thanh Thông <80942946+ITITSB19010@users.noreply.github.com>
2021-11-27 13:34:47 +0700

    commit

---
+++ PvZ/src/code/plant/Wallnut.java
@@ -0,0 +1,14 @@
+package code.plant;
+
+/**
+ * @author Đinh Bình Thanh Thông
+ */
+public class Wallnut extends Plant {
+    private static String name = "Wallnut";
+    private static int health = 500;
+
+    public Wallnut(int x, int y) {
+        super(x, y, name, speed, health, damage);
+        loadImage("/images/plants/wall_");
+    }
+}
