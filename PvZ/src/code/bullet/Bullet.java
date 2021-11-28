84b098a94f2eccf21a6e94d8e2d962c69dd943bf
vtenpo <phongzed2001@gmail.com>
2021-11-28 19:57:06 +0700

    commit

--- PvZ/src/code/bullet/Bullet.java
+++ PvZ/src/code/bullet/Bullet.java
@@ -3,14 +3,26 @@ package code.bullet;
 import code.Character;
 
 public class Bullet extends Character {
-    protected static int speed = 1;
+    protected static double speed = 1.25;
+    protected static int damage = 50;
 
-    public Bullet(int x, int y, String name, int speed, int health, int damage) {
-        super(x, y, name, speed, health, damage);
+    public Bullet(int x, int y) {
+        super(x, y);
     }
 
-    @Override
-    public void move() {
-        x += 1;
-    }  
+    public static double getSpeed() {
+        return speed;
+    }
+
+    public static void setSpeed(double speed) {
+        Bullet.speed = speed;
+    }
+
+    public static int getDamage() {
+        return damage;
+    }
+
+    public static void setDamage(int damage) {
+        Bullet.damage = damage;
+    }    
 }
--- PvZ/src/code/bullet/PeaBullet.java
+++ PvZ/src/code/bullet/PeaBullet.java
@@ -1,12 +1,8 @@
 package code.bullet;
 
 public class PeaBullet extends Bullet {
-    private static String name = "PeaBullet";
-    private static int damage = 50;
-
-    public PeaBullet(int x, int y, String name, int speed, int health, int damage) {
-        super(x, y, name, speed, health, damage);
-        loadImage("/images/bullet/pea_bullet.png");
-        getImageDimensions();
+    public PeaBullet(int x, int y) {
+        super(x, y);
+        loadImage("/images/bullet/pea.png");
     }
 }
